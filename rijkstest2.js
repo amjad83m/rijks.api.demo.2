0c53a9fd6bcf8a7b3805c3172eca54c816370fe8
amjad83m <amjad83m@hotmail.com>
2016-12-10 23:34:28 +0100

    modified responsiveness

--- rijkstest2.js
+++ rijkstest2.js
@@ -5,7 +5,7 @@
 
 // creating variables for required HTML elements on the page, the search text field, the search button, and the result div
 var searchBtn = document.getElementById("search");
-var resultD = document.getElementById("result");
+var resultDiv = document.getElementById("result-div");
 var searchField = document.getElementById("query");
 
 searchBtn.addEventListener("click", doSearch);
@@ -22,11 +22,11 @@ let artworkCounter = 0;
 function doSearch() {
     var searchString = searchField.value;
     if (!searchString) {
-        resultD.innerHTML = "The search field can't be empty!!!";
+        resultDiv.innerHTML = "<p>The search field can't be empty!!!</p>";
     } else {
         
         showElement("loader");
-        resultD.innerHTML = "";
+        resultDiv.innerHTML = "";
         resultsArray = [];
         
         // performing the search and getting the request from the server
@@ -47,10 +47,9 @@ function doSearch() {
 
 function displayResults() {
     resultsArray.forEach(function(result, index) {
-        resultD.innerHTML += "<div class='results'><ul class='results-ul'><li>Artwork #" + result.count + "</li><li>Title: " + result.title + "</li><li>Rijksmuseum Code: " + result.code + "</li><li>Artist (Maker): " + result.artist + "</li><li><a href=" + result.webpage + " target='_blank'>Go to artwork</a></li></ul></div>";
+        resultDiv.innerHTML += "<div class='results'><ul class='results-ul'><li>Artwork <p class='results-p'>#" + result.count + "</p></li><li>Title: <p class='results-p'>" + result.title + "</p></li><li>Rijksmuseum Code: <p class='results-p'>" + result.code + "</p></li><li>Artist (Maker): <p class='results-p'>" + result.artist + "</p></li></ul><p id='link-p'>Check it in <a href=" + result.webpage + " target='_blank'><img src='images/rijkslogo.jpg' id='rijks-logo'></a></p></div>";
     });
 }
-
 
 function showElement(elementId) {
     document.getElementById(elementId).style.display = "block";
